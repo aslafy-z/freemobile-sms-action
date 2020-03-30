2e1c40795d151bee224e936e925d460da64e399c
Zadkiel <hello@zadkiel.fr>
2020-03-30 20:33:59 +0200

    feat: add notification outcome

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-curl --get "https://smsapi.free-mobile.fr/sendmsg" \
+curl \
+  --silent \
+  --fail \
+  --get \
   --data-urlencode "user=${INPUT_FREEMOBILE_ID}" \
   --data-urlencode "pass=${INPUT_FREEMOBILE_TOKEN}" \
-  --data-urlencode "msg=${INPUT_MESSAGE}"
+  --data-urlencode "msg=${INPUT_MESSAGE}" \
+  "https://smsapi.free-mobile.fr/sendmsg" \
+  && echo "Notification has been sent." \
+  || (echo "Notification has failed. Check your credentials."; exit 1)
